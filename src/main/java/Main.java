52192bf184b501878122720f7cbab9862350e585
zyzz2011 <169068710+zyzz2011@users.noreply.github.com>
2024-09-02 12:02:32 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+ //Um programa que leia um valor inteiro N. Este valor será a quantidade de valores inteiros X que serão lidos em seguida. Mostre quantos destes valores X estão dentro do intervalo [10, 20] e quantos estão fora do intervalo, mostrando essas informações;
+int quantidadeDeNumerosDigitados =  
+ sc.nextInt();
+int valoresDentroDoIntervalo = 0;
+int valoresForaDoIntervalo = 0;
+int numeroDigitado;
+for(int i = 0; i <  quantidadeDeNumerosDigitados; i++) { 
+    
+  
+
+  
+
+
+
+    
+   sc.close();
+  }
+ 
+}
